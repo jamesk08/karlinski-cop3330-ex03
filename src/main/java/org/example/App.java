b80c9e6a48a169dc827f70e719ef1db738a3de95
jamesk08 <jameskarlinski@gmail.com>
2021-08-29 11:21:47 -0400

    adding quote printer maven project

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 James Karlinski
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println( "What is the quote?" );
+        String quote = scanner.nextLine();
+
+        System.out.println( "Who said it?" );
+        String author = scanner.nextLine();
+
+        System.out.println(author + " says, " + quote);
+    }
+}
